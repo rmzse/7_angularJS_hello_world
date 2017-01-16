19bc857fe82d5cce7bc46b5cba857ed36832c114
rmzse <rodrigo@therawdelicious.com>
2017-01-16 18:11:52 +0100

    move directive to separate file, according to rule of 1, IIFE and strict use, app still failing

---
+++ js/app.controller.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+    angular
+        .module("demoApp")
+        .directive("sayHello", sayHello);
+    
+    function sayHello() {
+        return {
+            scope: false,
+            link:
+
+                function ($scope, element, attrs) {
+                $scope.message = attrs.message;
+            },
+            template: "<h1>{{[message, user.firstName, user.lastName].join(' ') | uppercase }}!</h1>"
+        };
+    });
+})();
